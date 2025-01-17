26318f38bd6a783de8ac87e2638898225a9fc68a
Aromal0708 <aromalsunil12@gmail.com>
2025-01-17 21:49:04 +0530

    complete the hero setions and can move on the posters

--- app/(Root)/Components/Hero.tsx
+++ app/(Root)/Components/Hero.tsx
@@ -3,18 +3,20 @@ import { trending } from "@/utils/urls";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+//component
 const Hero = () => {
-  const url = trending;
   const [movie, setMovie] = useState<any[]>([]);
   const settings = {
     dots: true,
     infinite: true,
     speed: 800,
-    slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
-    arrows: true,
+    // arrows: true,
   };
 
   useEffect(() => {
@@ -23,32 +25,46 @@ const Hero = () => {
       if (!url) {
         return;
       }
-      const response = await axios.get(url + trending);
-      setMovie(response.data?.results);
+      try {
+        const response = await axios.get(`${url}${trending}`);
+        setMovie(response.data.results);
+        console.log(response.data.results);
+      } catch (error: any) {
+        console.error(error.message);
+      }
     };
     fetchData();
   }, []);
-  //this is today's stop :(   i haven't given up i will figt for what i want i will complete this project by the end of this week
   return (
-    <div
-      className=" h-[300px] w-full flex flex-col justify-center bg-cover bg bg-center items-start text-white p-1 lg:h-[430px]"
-      style={{
-        backgroundImage: `url("https://images.goodreturns.in/img/2025/01/yeh-jawaani-hy-deewani6-1736071788.jpg")`,
-      }}
-    >
-      <h1 className="text-xl ml-4 lg:font-bold sm:font-extrabold text-black">
-        Ye Jawani Hai Diwani
-      </h1>
-      <p className="text-black sm:text-[12px] sm:max-w-[250px] sm:font-extralight lg:font-normal lg:text-[16px] lg:mt-3 lg:max-w-[550px] ml-4 ">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus tempora
-        facilis impedit consequuntur provident adipisci a? Fuga optio ad est.
-        Reiciendis quasi in
-      </p>
-      <div className="flex flex-row mt-4 ml-4 gap-1">
-        <button className="btn">Play</button>
-        <button className="btn">Add to Watchlist</button>
-      </div>
-    </div>
+    <Slider {...settings} className="w-full overflow-hidden">
+      {movie.map((item: any) => (
+        <div
+          key={item.id}
+          className="relative h-[300px] w-full flex flex-col justify-center items-start text-white p-1 lg:h-[430px]"
+        >
+          <img
+            src={`https://image.tmdb.org/t/p/original${item.backdrop_path}`}
+            alt="image"
+            className="absolute top-0 left-0 w-full h-full object-fit object-center z-0"
+          />
+          <div className="absolute top-0 left-0 w-full h-full bg-black opacity-40 z-5"></div>
+
+          <div className="relative z-10 flex flex-col justify-between mt-28 ml-4 gap-4">
+            <h1 className="text-xl lg:font-bold sm:font-extrabold text-white">
+              {item.title}
+            </h1>
+            <p className="text-white sm:text-[12px] sm:max-w-[250px] sm:font-extralight lg:font-normal lg:text-[16px] lg:mt-3 lg:max-w-[550px] ">
+              {item.overview}
+            </p>
+          </div>
+
+          <div className="relative z-10 flex flex-row mt-4 ml-4 gap-1">
+            <button className="btn">Play</button>
+            <button className="btn">Add to Watchlist</button>
+          </div>
+        </div>
+      ))}
+    </Slider>
   );
 };
 
